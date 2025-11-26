73764b1799146ececbd197a25621b5bb7d9bee4d
Michal Kononenko <michalkononenko@gmail.com>
2025-11-25 22:52:43 -0500

    Added flake.nix and package.json

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs
+  } : let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    packages."x86_64-linux".default = pkgs.buildNpmPackage {
+      pname = "prompt-overlapper";
+      version = "0.0.1";
+      src = ./.;
+      npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+  };
+}
