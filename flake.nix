a5df25065354e70605972de0d45020d9d3001964
Michal Kononenko <michalkononenko@gmail.com>
2025-11-30 14:59:45 -0500

    Cleanup of flake.nix

--- flake.nix
+++ flake.nix
@@ -3,24 +3,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        packages = with pkgs; [
+          nodejs_20
+          typescript
+          nodePackages.npm
+        ];
+        app = pkgs.stdenv.mkDerivation {
+          name = "content-evaluator-dev";
+          version = "0.0.1";
+          src = gitignore.lib.gitignoreSource ./.;
+          buildInputs = packages;
+          buildPhase = ''
+            runHook preBuild
+            npm run build
+            runHook postBuild
+          '';
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            cp package.json $out/package.json
+            cp -r dist $out/dist
+            cp dist/index.js $out/bin/example-ts-nix
+            chmod a+x $out/bin/example-ts-nix
+
+            runHook postInstall
+          '';
+        };
       in
       {
+        defaultPackage = app;
         devShell = pkgs.mkShell {
           name = "content-evaluator-dev";
 
           # Packages necessary for developing a TypeScript/Angular app
-          packages = with pkgs; [
-            nodejs_20
-            typescript
-            nodePackages.npm
-          ];
-
+          packages = packages; 
           # Environment variables and setup commands
           shellHook = ''
             echo "--- Welcome to the Content Evaluator Dev Shell ---"
